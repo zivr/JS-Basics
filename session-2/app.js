8730f477ee9a1246bd5da4b4701c7e90625759de
502549131 <ziv.rosenzweig@ge.com>
2016-02-21 10:09:52 +0200

    session 2

--- session-2/app.js
+++ session-2/app.js
@@ -3,6 +3,31 @@ var TodoApp = TodoApp || {}; //namespace definition
 
 TodoApp.TodoItem = function (description){
     this.description = description;
+    this.date = new Date();
+    this._doneDate = null;
+    var done = false;
+
+    this.isDone = function () {
+        return done;
+    };
+    this.__defineSetter__("done", function(val){
+        done = val;
+        if (done){
+            this._doneDate = new Date();
+        }
+    });
+};
+
+
+TodoApp.TodoItem.prototype.toString = function() {
+    var result = 'Task ' + this.description + ' was created at ' + this.date.toLocaleString();
+    if (this.isDone()){
+        result += ' and finished at ' + this._doneDate.toLocaleTimeString();
+    }
+    else{
+        result += ' but still await for you to finish it';
+    }
+    return result;
 };
 
 TodoApp.TodoList = function (){
@@ -15,7 +40,7 @@ TodoApp.TodoList = function (){
 TodoApp.TodoList.prototype.toString = function () {
     var itemsList = [];
     this.items.forEach(function(item , idx){
-        itemsList.push('item [' + idx + ']: ' + JSON.stringify(item));
+        itemsList.push('item [' + idx + ']: ' + item);
     });
     return itemsList.join('\n');
 };
@@ -39,6 +64,11 @@ var myToDoApp = TodoApp.TodoFactory.createApp();
 var buyMilk = TodoApp.TodoFactory.createItem("buy milk");
 myToDoApp.Add(buyMilk);
 myToDoApp.Add(TodoApp.TodoFactory.createItem("read a book"));
+
+console.log('Have I bought milk ? ' + buyMilk.isDone());
+
+buyMilk.done = true;
+console.log('Have I bought milk ? ' + buyMilk.isDone());
 console.log(myToDoApp);
 
 
